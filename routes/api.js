292d3c1b3c7911851c8e89a18ccfbd54d4a955d9
Thor138 <tjcourey84@gmail.com>
2021-06-11 22:16:36 -0600

    finished adding routes to api.js

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { Workout } = require("../models");
 const db = require("../models");
 
 router.post("/api/workouts", (req, res) => {
@@ -16,6 +17,32 @@ router.put("/api/workouts/:id", (req, res) => {
     { _id: req.params.id },
     { $push: { exercises: req.body } }
   )
+    .then((dbWorkout) => {
+      res.status(200).json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/range", (req, res) => {
+  db.Workout.aggregate()
+    .addFields({
+      totalDuration: { $sum: "$exercises.duration" },
+    })
+    .then((dbWorkout) => {
+      res.status(200).json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/", (req, res) => {
+  db.Workout.aggregate()
+    .addFields({
+      totalDuration: { $sum: "$exercises.duration" },
+    })
     .then((dbWorkout) => {
       res.status(200).json(dbWorkout);
     })
